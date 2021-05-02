97cbe38dc6ca634e415d11a03bc9ddde09c81c4c
darkmagic9 <darkmagic9@gmail.com>
2021-05-02 15:57:31 +0700

    first commit

---
+++ ESP32_LCD_TEMP.ino
@@ -0,0 +1,98 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// DHT Sensor
+const int DHTPin = 4;
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+void setup()
+{  
+  // Initializing serial port for debugging purposes
+  Serial.begin(115200);
+  
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0); // กำหนดให้ เคอร์เซอร์ อยู่ตัวอักษรตำแหน่งที่0 แถวที่ 1 เตรียมพิมพ์ข้อความ
+  lcd.print("LCD1602 I2c Test"); //พิมพ์ข้อความ "LCD1602 I2c Test"
+  Serial.println("LCD1602 I2c Test");
+  lcd.setCursor(2, 1); // กำหนดให้ เคอร์เซอร์ อยู่ตัวอักษรกำแหน่งที3 แถวที่ 2 เตรียมพิมพ์ข้อความ
+  lcd.print("myarduino.net"); //พิมพ์ข้อความ "myarduino.net"
+  Serial.println("myarduino.net");
+  delay(10000);
+  lcd.clear();
+}
+void loop() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    strcpy(celsiusTemp, "Failed");
+    strcpy(fahrenheitTemp, "Failed");
+    strcpy(humidityTemp, "Failed");
+    lcd.clear();    
+  } else {
+    // Computes temperature values in Celsius + Fahrenheit and Humidity
+    float hic = dht.computeHeatIndex(t, h, false);
+    dtostrf(hic, 6, 2, celsiusTemp);
+    float hif = dht.computeHeatIndex(f, h);
+    dtostrf(hif, 6, 2, fahrenheitTemp);
+    dtostrf(h, 6, 2, humidityTemp);
+    // You can delete the following Serial.print's, it's just for debugging purposes
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.print(" *F");
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.println(" *F");
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("hum:     ");
+  lcd.setCursor(4, 0);
+  lcd.print(humidityTemp);
+  lcd.setCursor(9, 0);
+  lcd.print("%");
+  lcd.setCursor(0, 1);
+  lcd.print("Tem:     ");
+  lcd.setCursor(4, 1);
+  lcd.print(celsiusTemp);
+  lcd.setCursor(9, 1);
+  lcd.print("C");
+  
+  delay(500);
+}
